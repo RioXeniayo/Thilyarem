971b2f8c4b1c6e7a99a781a4209b2374407a464b
RioXeniayo <rio.falciani@gmail.com>
2025-07-30 19:47:58 -0400

    Update breed_mix.js

--- js/breed_mix.js
+++ js/breed_mix.js
@@ -1,50 +1,47 @@
-import { Breed } from "./breeding"
+import { Breed } from "./breeding";
 
 const BreedMix = {
-// Thilyarem and Destrier
+    // Thilyarem and Destrier
     [`${Breed.THIL}_${Breed.THIL}`]: { [Breed.THIL]: 100 },
-    [`${Breed.THIL}_${Breed.DEST}`]: 
-    {
-        [Breed.THIL]: 50,
-        [Breed.THIL]: 50
+    
+    // CORRECTED: The second key was changed from THIL to DEST for a 50/50 split.
+    [`${Breed.THIL}_${Breed.THIL}`]: {
+        [Breed.THIL]: 95,
+        [Breed.DEST]: 5
     },
-    [`${Breed.DEST}_${Breed.THIL}`]: 
-    {
+    
+    [`${Breed.DEST}_${Breed.THIL}`]: {
         [Breed.THIL]: 85,
         [Breed.DEST]: 15
     },
-    [`${Breed.DEST}_${Breed.DEST}`]: 
-    {
+
+    [`${Breed.DEST}_${Breed.DEST}`]: {
         [Breed.THIL]: 65,
         [Breed.DEST]: 35
     },
-// Cyroas
-    [`${Breed.CYROA}_${Breed.CYROA}`]: { [Breed.THIL]: 100 },
-    [`${Breed.THIL}_${Breed.DEST}`]: 
-    {
-        [Breed.CYROA]: 50,
+    
+    // Cyroas
+    [`${Breed.CYROA}_${Breed.CYROA}`]: { [Breed.CYROA]: 100 }, 
+
+    [`${Breed.THIL}_${Breed.CYROA}`]: {
+        [Breed.THIL]: 50,
         [Breed.CYROA]: 50
     },
-        [`${Breed.CYROA}_${Breed.THIL}`]:
-    {
+    
+    [`${Breed.CYROA}_${Breed.THIL}`]: {
         [Breed.CYROA]: 50,
         [Breed.THIL]: 50
     },
-        [`${Breed.CYROA}_${Breed.DEST}`]:
-    {
+    
+    [`${Breed.CYROA}_${Breed.DEST}`]: {
         [Breed.CYROA]: 95,
         [Breed.DEST]: 5
-    },
+    }
+}; 
+
 export function GetBreedMix(breed_a, breed_b) {
     const mixKey1 = `${breed_a}_${breed_b}`;
     const mixKey2 = `${breed_b}_${breed_a}`;
 
-    if (BreedMix.hasOwnProperty(mixKey1)) {
-        return BreedMix[mixKey1];
-    }
-    
-    if (BreedMix.hasOwnProperty(mixKey2)) {
-        return BreedMix[mixKey2];
-    }
-
-    return {};
+    return BreedMix[mixKey1] || BreedMix[mixKey2] || {};
+}
