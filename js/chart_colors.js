0875f324262f5dc0cda8177d256965c7fe597586
RioXeniayo <rio.falciani@gmail.com>
2025-07-30 03:35:50 -0400

    Create chart_colors.js

---
+++ js/chart_colors.js
@@ -0,0 +1,41 @@
+import { Breed } from "./breeding";
+
+const BaseColor = {
+    TAN: 'Tan',
+    TAN_BLUE: 'Tan',
+    TAN_DILUTE: 'Cream',
+    BROWN: 'Brown',
+    BROWN_BLUE: 'Brown',
+    BROWN_DILUTE: 'Lilac',
+    GREEN: 'Green',
+    GREEN_BLUE: 'Seagreen',
+    GREEN_DILUTE: 'Teagreen',
+    SLATE: 'Slate',
+    SLATE_BLUE: 'Blue',
+    SLATE_DILUTE: 'White',
+    BLACK: 'Black',
+};
+
+const BaseChartColor = {
+      [BaseColor.TAN]: ,
+    [BaseColor.TAN_BLUE]: ,
+    [BaseColor.TAN_DILUTE]: ,
+    [BaseColor.BROWN]: ,
+    [BaseColor.BROWN_BLUE]: ,
+    [BaseColor.BROWN_DILUTE]: ,
+    [BaseColor.GREEN]: ',
+    [BaseColor.GREEN_BLUE]: ,
+    [BaseColor.GREEN_DILUTE]: ,
+    [BaseColor.SLATE]: ,
+    [BaseColor.SLATE_BLUE]: ,
+    [BaseColor.SLATE_DILUTE]: ,
+    [BaseColor.BLACK]: 
+};
+
+const BreedChartColor = {
+    [Breed.THIL]: ,
+    [Breed.DEST]: ,
+    [Breed.CYROA]: ,
+};
+
+export { BaseChartColor, BreedChartColor }
