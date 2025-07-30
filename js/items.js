7f26735cb26a130ccce4d376f30ad91f02a929e1
RioXeniayo <rio.falciani@gmail.com>
2025-07-30 05:28:14 -0400

    Create items.js

---
+++ js/items.js
@@ -0,0 +1,14 @@
+const Herb = {
+    CRESHERB: 'Crescent Herb',
+    MOONHERB: 'Moonlite Herb',
+};
+
+const Potion = {
+    TESTPOT: 'Test Potion',
+};
+
+const StackingPotion = {
+    TESTPOT: 'Test Stack Potion',
+};
+
+export { Herb, Potion, StackingPotion }
