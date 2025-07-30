1f6ae769965e89f9d0693cdcfa71df58826fbb3c
RioXeniayo <rio.falciani@gmail.com>
2025-07-30 19:44:30 -0400

    Update breed_mix.js

--- js/breed_mix.js
+++ js/breed_mix.js
@@ -1,12 +1,12 @@
-import { Breed } from "./js/breeding"
+import { Breed } from "./breeding"
 
 const BreedMix = {
-    // Raptor
+// Thilyarem and Destrier
     [`${Breed.THIL}_${Breed.THIL}`]: { [Breed.THIL]: 100 },
-    [`${Breed.THIL}_${Breed.THIL}`]: 
+    [`${Breed.THIL}_${Breed.DEST}`]: 
     {
-        [Breed.THIL]: 95,
-        [Breed.DEST]: 5
+        [Breed.THIL]: 50,
+        [Breed.THIL]: 50
     },
     [`${Breed.DEST}_${Breed.THIL}`]: 
     {
@@ -18,7 +18,23 @@ const BreedMix = {
         [Breed.THIL]: 65,
         [Breed.DEST]: 35
     },
-
+// Cyroas
+    [`${Breed.CYROA}_${Breed.CYROA}`]: { [Breed.THIL]: 100 },
+    [`${Breed.THIL}_${Breed.DEST}`]: 
+    {
+        [Breed.CYROA]: 50,
+        [Breed.CYROA]: 50
+    },
+        [`${Breed.CYROA}_${Breed.THIL}`]:
+    {
+        [Breed.CYROA]: 50,
+        [Breed.THIL]: 50
+    },
+        [`${Breed.CYROA}_${Breed.DEST}`]:
+    {
+        [Breed.CYROA]: 95,
+        [Breed.DEST]: 5
+    },
 export function GetBreedMix(breed_a, breed_b) {
     const mixKey1 = `${breed_a}_${breed_b}`;
     const mixKey2 = `${breed_b}_${breed_a}`;
