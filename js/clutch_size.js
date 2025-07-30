0dc13f45959a33155ab006e3818d58bf645aa3fd
RioXeniayo <rio.falciani@gmail.com>
2025-07-30 03:21:43 -0400

    Create clutch_size.js

---
+++ js/clutch_size.js
@@ -0,0 +1,15 @@
+import { Breed } from "./breeding";
+
+const ClutchMin = {
+    [Breed.THIL]: 1,
+    [Breed.DEST]: 1,
+    [Breed.CYROA]: 1,
+};
+
+const ClutchMax = {
+    [Breed.THIL]: 5,
+    [Breed.DEST]: 3,
+    [Breed.CYROA]: 5,
+};
+
+export { ClutchMin, ClutchMax }
